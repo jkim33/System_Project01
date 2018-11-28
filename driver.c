2fd994b9d38441cdc098ecd2c186a94ac7d68433
jkim33 <jkim33@stuy.edu>
2018-11-28 11:21:41 -0500

    stuff

--- driver.c
+++ driver.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+//separates the string based on the semicolons
+//returns an array of executable strings
 char** separate(char* input) {
   char ** ret = malloc(128 * sizeof(char *));
   int i = 0;
@@ -24,6 +26,28 @@ char** separate(char* input) {
   return ret;
 }
 
+//used to separate executable strings with >, <, or |
+char** separateinput(char *input) {
+  char ** ret = malloc(2 * sizeof(char *));
+  int i = 0;
+  while(input) {
+    ret[i] = strsep(&input, "><|");
+    i++;
+  }
+  i = 0;
+  while (ret[i]) {
+    if (strncmp(ret[i], " ", 1) == 0) {
+      ret[i] = &ret[i][1];
+    }
+    if (strncmp(&ret[i][strlen(ret[i])-1], " ", 1) == 0) {
+      ret[i][strlen(ret[i])-1] = 0;
+    }
+    i++;
+  }
+  return ret;
+}
+
+//used to change directories (cd)
 void changedir(char* input) {
   int f = fork();
   if (f) {
@@ -36,6 +60,7 @@ void changedir(char* input) {
   return;
 }
 
+//executes all executable strings that don't involve a redirect or a pipe
 void execute(char* input) {
   if (strncmp(input, "cd", 2) == 0) {
     char* new = &input[3];
@@ -59,6 +84,43 @@ void execute(char* input) {
   return;
 }
 
+void redirectout(char *input) {
+  char** separated = malloc(2 * sizeof(char *));
+  int fd = open(separated[1], O_CREAT | O_WRONLY, 0644);
+}
+
+void redirectin(char *input) {
+  
+}
+
+void pipe(char *input) {
+
+}
+
+//checks if the executable string has a redirect or a pipe
+//0 = None
+//1 = >
+//2 = <
+//3 = |
+int check(char* input) {
+  char *found = strpbrk(input, "><|");
+  if (found) {
+    if (strncmp(found, ">", 1) == 0) {
+      return 1;
+    }
+    if (strncmp(found, "<", 1) == 0) {
+      return 2;
+    }
+    if (strncmp(found, "|", 1) == 0) {
+      return 3;
+    }
+  }
+  else {
+    return 0;
+  }
+}
+
+//runs the shell
 int main() {
   while (1) {
     printf("Write a command: ");
@@ -69,7 +131,19 @@ int main() {
     separated = separate(input);
     int i = 0;
     while (separated[i]) {
-      execute(separated[i]);
+      int c = check(separated[i]);
+      if (!c) {
+	execute(separated([i]));
+      }
+      else if (c == 1) {
+	redirectout(separated[i]);
+      }
+      else if (c == 2) {
+	redirectin(separated[i]);
+      }
+      else if (c == 3) {
+	pipe(separated[i]);
+      }
       i++;
     }
   }
